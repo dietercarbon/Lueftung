4569b91e91e77f2bcfcac337d04624cefc46cd14
Dieter Carbon <dieter.carbon@comidio.de>
2025-02-02 15:23:02 +0100

    Add files via upload

---
+++ Zugang.py
@@ -0,0 +1,13 @@
+def wlanSSID():
+    #return "TP-Link"
+    return "Node-RED_Demo"
+
+def wlanPW():
+    return "picobello"
+
+def IP_MQTT_broker():
+    return "192.168.0.233"
+    #return "192.168.0.102
+
+
+
